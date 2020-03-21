57da6398516c4b9f0ead3aab274f8392e729ff72
袁昊 <52455938+Ricky846@users.noreply.github.com>
2020-03-21 20:10:39 +0800

    Update accounts.go

--- go-ethereum-release-1.9/accounts/accounts.go
+++ go-ethereum-release-1.9/accounts/accounts.go
@@ -30,11 +30,14 @@ import (
 
 // Account represents an Ethereum account located at a specific location defined
 // by the optional URL field.
+//以太坊地址类型结构
 type Account struct {
+//把struct编码成json字符串时，common.Address字段的key是address
 	Address common.Address `json:"address"` // Ethereum account address derived from the key
+//把struct编码成json字符串时，URL字段的key是url
 	URL     URL            `json:"url"`     // Optional resource locator within a backend
-} //以太坊地址类型结构
-
+} 
+//一些常量的定义(但没搞清楚这些常量的作用，未在代码中找到这些常量的引用）
 const (
 	MimetypeDataWithValidator = "data/validator"
 	MimetypeTypedData         = "data/typed"
@@ -49,12 +52,12 @@ type Wallet interface {
 	// URL retrieves the canonical path under which this wallet is reachable. It is
 	// user by upper layers to define a sorting order over all wallets from multiple
 	// backends.
-	URL() URL  //获取该钱包可以访问的规范路径
+	URL() URL //获取该钱包可以访问的规范路径
 
 	// Status returns a textual status to aid the user in the current state of the
 	// wallet. It also returns an error indicating any failure the wallet might have
 	// encountered.
-	Status() (string, error)  //钱包的状态
+	Status() (string, error)  //钱包状态
 
 	// Open initializes access to a wallet instance. It is not meant to unlock or
 	// decrypt account keys, rather simply to establish a connection to hardware
@@ -67,23 +70,24 @@ type Wallet interface {
 	//
 	// Please note, if you open a wallet, you must close it to release any allocated
 	// resources (especially important when working with hardware wallets).
-	Open(passphrase string) error
+	Open(passphrase string) error  //初始化对钱包实例的访问
 
 	// Close releases any resources held by an open wallet instance.
-	Close() error
+	Close() error  //释放open方法占用的资源
 
 	// Accounts retrieves the list of signing accounts the wallet is currently aware
 	// of. For hierarchical deterministic wallets, the list will not be exhaustive,
 	// rather only contain the accounts explicitly pinned during account derivation.
-	Accounts() []Account
+	Accounts() []Account  //获取账号列表
 
 	// Contains returns whether an account is part of this particular wallet or not.
-	Contains(account Account) bool
+	Contains(account Account) bool  //查询指定账户是否属于该钱包
+
 
 	// Derive attempts to explicitly derive a hierarchical deterministic account at
 	// the specified derivation path. If requested, the derived account will be added
 	// to the wallet's tracked account list.
-	Derive(path DerivationPath, pin bool) (Account, error)
+	Derive(path DerivationPath, pin bool) (Account, error) //专门给确定性钱包使用的
 
 	// SelfDerive sets a base account derivation path from which the wallet attempts
 	// to discover non zero accounts and automatically add them to list of tracked
@@ -99,7 +103,7 @@ type Wallet interface {
 	//
 	// You can disable automatic account discovery by calling SelfDerive with a nil
 	// chain state reader.
-	SelfDerive(bases []DerivationPath, chain ethereum.ChainStateReader)
+	SelfDerive(bases []DerivationPath, chain ethereum.ChainStateReader) //设置一个账户导出路径
 
 	// SignData requests the wallet to sign the hash of the given data
 	// It looks up the account specified either solely via its address contained within,
@@ -146,14 +150,17 @@ type Wallet interface {
 	// about which fields or actions are needed. The user may retry by providing
 	// the needed details via SignTxWithPassphrase, or by other means (e.g. unlock
 	// the account in a keystore).
+	//请求钱包为传入的交易进行签名
 	SignTx(account Account, tx *types.Transaction, chainID *big.Int) (*types.Transaction, error)
-
+	//请求钱包使用指定的passphrase为传入的哈希进行签名
 	// SignTxWithPassphrase is identical to SignTx, but also takes a password
 	SignTxWithPassphrase(account Account, passphrase string, tx *types.Transaction, chainID *big.Int) (*types.Transaction, error)
+	
 }
 
 // Backend is a "wallet provider" that may contain a batch of accounts they can
 // sign transactions with and upon request, do so.
+//钱包的后端实现(服务)，主要实现keystore钱包以及USB硬件钱包
 type Backend interface {
 	// Wallets retrieves the list of wallets the backend is currently aware of.
 	//
@@ -165,10 +172,12 @@ type Backend interface {
 	// URL assigned by the backend. Since wallets (especially hardware) may come and
 	// go, the same wallet might appear at a different positions in the list during
 	// subsequent retrievals.
+	//获取钱包列表
 	Wallets() []Wallet
 
 	// Subscribe creates an async subscription to receive notifications when the
 	// backend detects the arrival or departure of a wallet.
+	//订阅钱包相关事件
 	Subscribe(sink chan<- WalletEvent) event.Subscription
 }
 
@@ -200,15 +209,18 @@ func TextAndHash(data []byte) ([]byte, string) {
 
 // WalletEventType represents the different event types that can be fired by
 // the wallet subscription subsystem.
+//钱包事件的类型
 type WalletEventType int
 
 const (
 	// WalletArrived is fired when a new wallet is detected either via USB or via
 	// a filesystem event in the keystore.
+	//在通过USB或者密码文件检测到新钱包的时候，会触发该事件
 	WalletArrived WalletEventType = iota
 
 	// WalletOpened is fired when a wallet is successfully opened with the purpose
 	// of starting any background processes such as automatic key derivation.
+	//开户钱包所触发的事件
 	WalletOpened
 
 	// WalletDropped
@@ -217,6 +229,7 @@ const (
 
 // WalletEvent is an event fired by an account backend when a wallet arrival or
 // departure is detected.
+//在检测到钱包账户发生改变时所触发的事件
 type WalletEvent struct {
 	Wallet Wallet          // Wallet instance arrived or departed
 	Kind   WalletEventType // Event type that happened in the system
