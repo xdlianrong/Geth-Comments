d4dce8e94f2b1cb19c956a56b227e8ba66675510
xzy <3020837723@qq.com>
2020-11-20 19:55:26 +0800

    add sm2-generatekey-api

--- go-ethereum-release-1.9/crypto/crypto.go
+++ go-ethereum-release-1.9/crypto/crypto.go
@@ -32,6 +32,8 @@ import (
 	"github.com/ethereum/go-ethereum/common/math"
 	"github.com/ethereum/go-ethereum/rlp"
 	"golang.org/x/crypto/sha3"
+
+	"github.com/ethereum/go-ethereum/crypto/sm2"
 )
 
 //SignatureLength indicates the byte length required to carry a signature with recovery id.
@@ -46,6 +48,9 @@ const DigestLength = 32
 var (
 	secp256k1N, _  = new(big.Int).SetString("fffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141", 16)
 	secp256k1halfN = new(big.Int).Div(secp256k1N, big.NewInt(2))
+
+	sm2P256N, _    = new(big.Int).SetString("FFFFFFFEFFFFFFFFFFFFFFFFFFFFFFFF7203DF6B21C6052B53BBF40939D54123", 16)
+	sm2P256halfN   = new(big.Int).Div(sm2P256N, big.NewInt(2))
 )
 
 var errInvalidPubkey = errors.New("invalid secp256k1 public key")
@@ -96,11 +101,22 @@ func ToECDSA(d []byte) (*ecdsa.PrivateKey, error) {
 	return toECDSA(d, true)
 }
 
+// 通过现有编码标准生成SM2公私钥
+func ToSM2(d []byte) (*sm2.PrivateKey, error) {
+	return toSM2(d, true)
+}
+
 // ToECDSAUnsafe blindly converts a binary blob to a private key. It should almost
 // never be used unless you are sure the input is valid and want to avoid hitting
 // errors due to bad origin encoding (0 prefixes cut off).
 func ToECDSAUnsafe(d []byte) *ecdsa.PrivateKey {
 	priv, _ := toECDSA(d, false)
+	return priv
+}
+
+// 通过原来编码标准生成SM2公私钥（0前缀）
+func ToSM2Unsafe(d []byte) *sm2.PrivateKey {
+	priv, _ := toSM2(d, false)
 	return priv
 }
 
@@ -131,8 +147,41 @@ func toECDSA(d []byte, strict bool) (*ecdsa.PrivateKey, error) {
 	return priv, nil
 }
 
+// 根据提供的参数生成sm2公私钥
+func toSM2(d []byte, strict bool) (*sm2.PrivateKey, error) {
+	priv := new(sm2.PrivateKey)
+	priv.PublicKey.Curve = Sm2p256()
+	if strict && 8*len(d) != priv.Params().BitSize {
+		return nil, fmt.Errorf("invalid length, need %d bits", priv.Params().BitSize)
+	}
+	priv.D = new(big.Int).SetBytes(d)
+
+	// The priv.D must < N
+	if priv.D.Cmp(sm2P256N) >= 0 {
+		return nil, fmt.Errorf("invalid private key, >=N")
+	}
+	// The priv.D must not be zero or negative.
+	if priv.D.Sign() <= 0 {
+		return nil, fmt.Errorf("invalid private key, zero or negative")
+	}
+
+	priv.PublicKey.X, priv.PublicKey.Y = priv.PublicKey.Curve.ScalarBaseMult(d)
+	if priv.PublicKey.X == nil {
+		return nil, errors.New("invalid private key")
+	}
+	return priv, nil
+}
+
 // FromECDSA exports a private key into a binary dump.
 func FromECDSA(priv *ecdsa.PrivateKey) []byte {
+	if priv == nil {
+		return nil
+	}
+	return math.PaddedBigBytes(priv.D, priv.Params().BitSize/8)
+}
+
+// 将SM2私钥转换为字节数组类型
+func FromSM2(priv *sm2.PrivateKey) []byte {
 	if priv == nil {
 		return nil
 	}
@@ -148,11 +197,28 @@ func UnmarshalPubkey(pub []byte) (*ecdsa.PublicKey, error) {
 	return &ecdsa.PublicKey{Curve: S256(), X: x, Y: y}, nil
 }
 
+// 将非序列化的SM2公钥字节数组转换为椭圆曲线库标准的公钥
+func UnmarshalSM2Pubkey(pub []byte) (*sm2.PublicKey, error) {
+	x, y := elliptic.Unmarshal(Sm2p256(), pub)
+	if x == nil {
+		return nil, errInvalidPubkey
+	}
+	return &sm2.PublicKey{Curve: Sm2p256(), X: x, Y: y}, nil
+}
+
 func FromECDSAPub(pub *ecdsa.PublicKey) []byte {
 	if pub == nil || pub.X == nil || pub.Y == nil {
 		return nil
 	}
 	return elliptic.Marshal(S256(), pub.X, pub.Y)
+}
+
+// 通过SM2公钥信息和椭圆曲线参数生成指定长度字节数组的算法
+func FromSM2Pub(pub *sm2.PublicKey) []byte {
+	if pub == nil || pub.X == nil || pub.Y == nil {
+		return nil
+	}
+	return elliptic.Marshal(Sm2p256(), pub.X, pub.Y)
 }
 
 // HexToECDSA parses a secp256k1 private key.
@@ -162,6 +228,15 @@ func HexToECDSA(hexkey string) (*ecdsa.PrivateKey, error) {
 		return nil, errors.New("invalid hex string")
 	}
 	return ToECDSA(b)
+}
+
+// 通过十六进制字符串生成sm2公私钥
+func HexToSM2(hexkey string) (*sm2.PrivateKey, error) {
+	b, err := hex.DecodeString(hexkey)
+	if err != nil {
+		return nil, errors.New("invalid hex string")
+	}
+	return ToSM2(b)
 }
 
 // LoadECDSA loads a secp256k1 private key from the given file.
@@ -183,6 +258,26 @@ func LoadECDSA(file string) (*ecdsa.PrivateKey, error) {
 	return ToECDSA(key)
 }
 
+// 从文件中的指定key加载sm2私钥
+func LoadSM2(file string) (*sm2.PrivateKey, error) {
+	buf := make([]byte, 64)
+	fd, err := os.Open(file)
+	if err != nil {
+		return nil, err
+	}
+	defer fd.Close()
+	if _, err := io.ReadFull(fd, buf); err != nil {
+		return nil, err
+	}
+
+	key, err := hex.DecodeString(string(buf))
+	if err != nil {
+		return nil, err
+	}
+	return ToSM2(key)
+}
+
+
 // SaveECDSA saves a secp256k1 private key to the given file with
 // restrictive permissions. The key data is saved hex-encoded.
 func SaveECDSA(file string, key *ecdsa.PrivateKey) error {
@@ -190,12 +285,24 @@ func SaveECDSA(file string, key *ecdsa.PrivateKey) error {
 	return ioutil.WriteFile(file, []byte(k), 0600)
 }
 
+//把sm2私钥写入文件指定文件
+func SaveSM2(file string, key *sm2.PrivateKey) error {
+	k := hex.EncodeToString(FromSM2(key))
+	return ioutil.WriteFile(file, []byte(k), 0600)
+}
+
 func GenerateKey() (*ecdsa.PrivateKey, error) {
 	return ecdsa.GenerateKey(S256(), rand.Reader)
 }
 
+// 不指定参数生成一对sm2公私钥
+func GenerateSM2Key() (*sm2.PrivateKey, error) {
+	return sm2.GenerateKey()
+}
+
 // ValidateSignatureValues verifies whether the signature values are valid with
 // the given chain rules. The v value is assumed to be either 0 or 1.
+// TODO: 修改SM2验签算法对于参数v，r，s的验证
 func ValidateSignatureValues(v byte, r, s *big.Int, homestead bool) bool {
 	if r.Cmp(common.Big1) < 0 || s.Cmp(common.Big1) < 0 {
 		return false
@@ -211,6 +318,12 @@ func ValidateSignatureValues(v byte, r, s *big.Int, homestead bool) bool {
 
 func PubkeyToAddress(p ecdsa.PublicKey) common.Address {
 	pubBytes := FromECDSAPub(&p)
+	return common.BytesToAddress(Keccak256(pubBytes[1:])[12:])
+}
+
+func Sm2PubkeyToAddress(p sm2.PublicKey) common.Address {
+	pubBytes := FromSM2Pub(&p)
+	//TODO: 后面将keccak256算法替换为SM3
 	return common.BytesToAddress(Keccak256(pubBytes[1:])[12:])
 }
 
