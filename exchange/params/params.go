6e9ea496afc13c0703f6d6f0283ef25ae873a8a2
xiezy <3020837723@qq.com>
2021-01-12 22:10:16 -0800

    recover

--- exchange/params/params.go
+++ exchange/params/params.go
@@ -2,13 +2,13 @@ package params
 
 //url
 var (
-	//Verifyurl = "http://39.99.227.43:1423/verify"
-	//Getpuburl = "http://39.99.227.43:1423/regkey?chainID=1"
-	//Ethurl = "http://39.105.58.136:8545"
+	Verifyurl = "http://39.99.227.43:1423/verify"
+	Getpuburl = "http://39.99.227.43:1423/regkey?chainID=1"
+	Ethurl = "http://39.105.58.136:8545"
 
-	Verifyurl = "http://localhost:1423/verify"
-	Getpuburl = "http://localhost:1423/regkey?chainID=1"
-	Ethurl = "http://localhost:8545"
+	//Verifyurl = "http://localhost:1423/verify"
+	//Getpuburl = "http://localhost:1423/regkey?chainID=1"
+	//Ethurl = "http://localhost:8545"
 )
 
 // address
